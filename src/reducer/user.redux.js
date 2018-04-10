a97b499f6ed93aad2c908ac4764379c1f3ebe58c
Lentaro <qq934884267@gmail.com>
2018-04-10 23:50:40 +0800

    add map

--- src/reducer/user.redux.js
+++ src/reducer/user.redux.js
@@ -1,21 +1,21 @@
-import {fromJS} from 'immutable'
-import moment from 'moment'
+import { fromJS, Map } from "immutable";
+import moment from "moment";
 
 // action
 const LOAD_DATA = "LOAD_DATA";
 
 // reducer
-const initState = {
-  username:'',
-  nickname:'',
-  avatar:'',
-  desc:'',
-  sex:'',
-  follow:[],
-  fans:[],
-  blog:[],
-  birthday:moment('1980/1/1','YYYY/MM/DD')
-};
+const initState = Map({
+  username: "",
+  nickname: "",
+  avatar: "",
+  desc: "",
+  sex: "", 
+  follow: [],
+  fans: [],
+  blog: [],
+  birthday: moment("1980/1/1", "YYYY/MM/DD")
+});
 
 export const user = (state = fromJS(initState), action) => {
   switch (action.type) {
